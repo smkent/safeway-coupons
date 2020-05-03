633d88b9862860721abeee122930b084ad32ab91
Will Duff <widuff@microsoft.com>
2020-05-03 16:03:29 -0700

    Declare parameters correctly

--- AzureFunctions/ClipCoupons/__init__.py
+++ AzureFunctions/ClipCoupons/__init__.py
@@ -4,14 +4,14 @@ import logging
 import azure.functions as func
 from . import safeway_coupons
 
-def main(mytimer: func.TimerRequest) -> None:
+def main(ClipCouponsTimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
-    if mytimer.past_due:
+    if ClipCouponsTimer.past_due:
         logging.info('The timer is past due!')
     
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
-    
+
     safeway_coupons.run()
 
