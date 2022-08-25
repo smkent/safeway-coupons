bc5435d7669bb0a9c4c1ecbada9c4b081af77d09
Stephen Kent <smkent@smkent.net>
2022-08-24 19:48:09 -0700

    Fixup delay time computation

--- safeway_coupons/utils.py
+++ safeway_coupons/utils.py
@@ -128,15 +128,15 @@ def yield_delay(
 ) -> Generator[T, None, None]:
     for count, item in enumerate(iterable):
         delay_time = 0.0
-        if sleep_level < 1:
-            if count and count % 12 == 0:
+        if sleep_level < 2:
+            if sleep_level < 1 and count and count % 12 == 0:
                 if count % 48 == 0:
                     delay_time = random.uniform(15.0, 25.0)
                 else:
                     delay_time = random.uniform(4.0, 8.0)
                 delay_time = round(delay_time, 2)
-        elif sleep_level < 2:
-            delay_time = random.uniform(0.3, 0.8)
+            else:
+                delay_time = random.uniform(0.3, 0.8)
         if delay_time:
             if debug_level >= 1 and delay_time >= 1:
                 print(f"Waiting {delay_time} seconds")
