1fdb0214f2143ec70f6f1159f4b931486572eafc
Stephen Kent <smkent@smkent.net>
2022-08-24 20:06:54 -0700

    Rework mocks using pytest-mock

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,37 +1,25 @@
 import json
 import os
 import sys
-from typing import Iterable
-from unittest import mock
 
-import pytest
 import responses
+from pytest_mock import MockerFixture
 
 from safeway_coupons.app import main
 
 
-@pytest.fixture
-def mock_account_env() -> Iterable[None]:
-    with mock.patch.object(
+def test_main(
+    mocker: MockerFixture, http_responses: responses.RequestsMock
+) -> None:
+    mocker.patch.object(
         os,
         "environ",
         {
             "SAFEWAY_COUPONS_USERNAME": "ness@onett.example",
             "SAFEWAY_COUPONS_PASSWORD": "pk_fire",
         },
-    ):
-        yield
-
-
-@pytest.fixture
-def mock_argv() -> Iterable[None]:
-    with mock.patch.object(sys, "argv", ["safeway-coupons"]):
-        yield
-
-
-@pytest.mark.usefixtures("mock_account_env")
-@pytest.mark.usefixtures("mock_argv")
-def test_main(http_responses: responses.RequestsMock) -> None:
+    )
+    mocker.patch.object(sys, "argv", ["safeway-coupons"])
     http_responses.add(
         method=responses.POST,
         url="https://albertsons.okta.com/api/v1/authn",
